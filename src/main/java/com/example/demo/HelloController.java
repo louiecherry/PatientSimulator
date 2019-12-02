071eb62ba5c0ca5de46eef51da250d2a99dbc23d
ralph <ralphrobertsuk@gmail.com>
2019-12-02 12:52:33 +0000

    added a form on /greeting, will set up to let choose headers soon

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.demo;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String helloController() {
+   /* @RequestMapping("/")
+    public String helloController()  {
         return "<b>Welcome to Patient Simulator</b>";
+    }*/
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 
 }
