b6505fde8ccf1f44f9656885e3b502fdd5c6584d
Rob Russmann <rob.russmann@gmail.com>
2019-12-08 00:07:59 +0000

    Now shows data from CSV in tabular form. Also replays data from CSV using justGage gauge widgets. Uses cosine interpolation between datapoints (needs checking) for smoother playback. Also added basic clock (scroll to bottom of page). UI needs a *lot* of cleanup and needs further features (see Jira).

--- src/main/java/com/patient/simulators/ReadCsvFile.java
+++ src/main/java/com/patient/simulators/ReadCsvFile.java
@@ -1,6 +1,8 @@
 package com.patient.simulators;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,11 +11,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ReadCsvFile {
+
+@SpringBootApplication
+public class ReadCsvFile extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadCsvFile.class, args);
         System.out.println( "Read CSV running");
+        System.out.println(System.getProperty("user.dir"));
         readInCsv();
     }
 
@@ -22,10 +27,10 @@ public class ReadCsvFile {
         List<String>Headers = new ArrayList<>();
         String csvFile = pathFile;
         String line = "";
-        String cvsSplitBy = ",";
+        String csvSplitBy = ",";
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             line = br.readLine();
-            String[] data = line.split(cvsSplitBy);
+            String[] data = line.split(csvSplitBy);
             Headers.addAll(Arrays.asList(data));
         }
         catch (IOException e) {
@@ -34,13 +39,14 @@ public class ReadCsvFile {
         return Headers;
     }
     public static void readInCsv(){
-        String csvFile = "./src/main/java/com/example/demo/csv/2019-10-28_1030_PhysiologicalDataLog.csv";
+        String csvFile = "./src/main/java/com/patient/simulators/csv/2019-10-28_1030_PhysiologicalDataLog.csv";
+        //String csvFile = "./csv/2019-10-28_1030_PhysiologicalDataLog.csv";
         String line = "";
-        String cvsSplitBy = ",";
+        String csvSplitBy = ",";
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(cvsSplitBy);
+                String[] data = line.split(csvSplitBy);
                 System.out.println(data[0] + data[3] + data[4] + data[5] + data[6] + data[7]);
             }
         } catch (IOException e) {
